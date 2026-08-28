c9dc869649228c24a1d4b7665b72fdf0c10ed946
Pedro Freire <pedrofreire10out@gmail.com>
2026-08-27 21:32:58 -0400

    Criação do exercício 5

---
+++ ex5-cepef.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// EXERCÍCIO 5 - DIVISÃO SEGURA
+int main()
+{
+    double n1, n2, divisao;
+
+    cout << "Digite o primeiro valor: ";
+    cin >> n1;
+
+    cout << "Digite o segundo valor: ";
+    cin >> n2;
+
+    return 0;
+}
